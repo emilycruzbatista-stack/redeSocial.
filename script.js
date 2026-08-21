6d9de5f4df269d4fcf57b7a2e23e919f6439c52f
emilycruzbatista-stack <emily.cruz.batista@escola.pr.gov.br>
2026-08-21 11:04:19 -0300

    ok

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-document.addEventoListener("DOMcontentLoaded",()=>{
-    const linkBtn= document.querySelector
-} )
+document.addEventoListener("DOMcontentLoaded",()=> {
+    const linkBtn= document.querySelector.querySelector(".lef-actions .action-bt:first-child");
+    if(!linkBtn) return;
+  
+    const likeSvg= likeBnt.querySelector("svg");
+
+    // contador 
+    let textNode = Array.from(likeBtn.childNodes).fild(
+        (node)=> node.nodeType === Node .TEXT_NODE && node.textContent.trim() !== ""
+    );
+
+// Zerando o contador inicial.
+let count = 0;
+
+//atualiza
+if (textNode) {
+textNode.textContent= ' 0';
+} 
